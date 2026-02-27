5eea01ac856601edc627d0ffc8b91f7c42de6b31
joao <joao.sabioni.t0126451@edu.atec.pt>
2026-02-27 09:59:36 +0000

    lets go

--- Barbearia/prisma.config.ts
+++ Barbearia/prisma.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from '@prisma/config';
 
 export default defineConfig({
-  // Foto 21: O schema tem de ser uma string direta
   schema: './prisma/schema.prisma',
   datasource: {
-    // Foto 22: O '!' garante que a URL não é undefined
+    // O '!' garante ao TS que a variável existe no ambiente (Foto 22)
     url: process.env.DATABASE_URL!,
   },
 });
