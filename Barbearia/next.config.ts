5603c3769bf9210f93580e9526d935e2ab67133d
joao <joao.sabioni.t0126451@edu.atec.pt>
2026-03-03 13:05:38 +0000

    fix: upgrade next.js

--- Barbearia/next.config.ts
+++ Barbearia/next.config.ts
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   productionBrowserSourceMaps: false,
+  serverExternalPackages: ['@prisma/client', 'prisma'],
   async redirects() {
     return [
       {
